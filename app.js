70516684ad0b4a2ea0f8fd579f346b604dc505a7
Flávio Resende <flv.2017@gmail.com>
2025-01-28 14:44:10 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,75 @@
+let listaDeNumerosSorteados =[];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag,texto){
+    let paragrafo = document.querySelector(tag);
+    paragrafo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female',{rate:1.2})
+}
+
+
+exibirTextoNaTela('h1','Jogo do número secreto');
+exibirTextoNaTela('p','Escolha um número entre 1 e 10');
+
+function verificarChute(){
+    //aqui vai retornar true ou false que é o tipo booleano
+    let chute = document.querySelector('input').value;
+    if(chute == numeroSecreto){
+        exibirTextoNaTela('h1','Acertou!');
+        let palavraTentativa = tentativas > 1? 'tentativas': 'tentativa';
+        let mensagemTentativas= `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p',mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
+    }else{
+        if(chute>numeroSecreto){
+            exibirTextoNaTela('p','O número secreto é menor');
+        }else{
+            exibirTextoNaTela('p','O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+        
+        
+    }
+    
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);  // Gera um número aleatório
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+    if(quantidadeDeElementosNaLista == numeroLimite){
+        listaDeNumerosSorteados=[];
+
+    }
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();  // Recursão ocorre aqui, mas agora o valor gerado é retornado corretamente
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);  // Adiciona o número sorteado à lista
+        console.log(listaDeNumerosSorteados);  // Para debug, veja os números sorteados
+        return numeroEscolhido;  // Retorna o número gerado corretamente
+    }
+}
+
+// se vc errou o campo fica vazio, não fica aparecendo o valor do chute que vc deu
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+    
+ }
+ 
+ function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled',true);
+ }
+
+ function exibirMensagemInicial() {
+    exibirTextoNaTela('h1','Jogo do número secreto');
+    exibirTextoNaTela('p','Escolha um número entre 1 e 10');
+    
+ }
